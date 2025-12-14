38f1ed8e5d70cad2af04c664f914d6711b61ce89
jonathan_fambro <startek591@gmail.com>
2025-12-14 15:57:29 -0500

    Defining a component

--- react/first-component/src/App.js
+++ react/first-component/src/App.js
@@ -1,16 +1,11 @@
 import "./App.css";
 
+export function Profile() {
+  return <img src="https://i.imgur.com/MK3eW3Am.jpg" alt="Katherine Johnson" />;
+}
+
 function App() {
-  return (
-    <article>
-      <h1>My First Component</h1>
-      <ol>
-        <li>Components: UI Building Blocks</li>
-        <li>Defining a Component</li>
-        <li>Using a Component</li>
-      </ol>
-    </article>
-  );
+  return <Profile />;
 }
 
 export default App;
