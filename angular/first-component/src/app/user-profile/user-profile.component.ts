e1bd60d59b131bf80883c596ba80dc6c43e38641
jonathan_fambro <startek591@gmail.com>
2025-12-14 16:40:26 -0500

    Defining a component

---
+++ angular/first-component/src/app/user-profile/user-profile.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'user-profile',
+  standalone: true,
+  template: `
+    <h1>User profile</h1>
+    <p>This is the user profile page</p>
+  `,
+})
+export class UserProfile {}
