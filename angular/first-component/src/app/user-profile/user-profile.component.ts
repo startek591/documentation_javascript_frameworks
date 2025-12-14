9bd046db523e9203651a2b9b0712c927dbff69cc
jonathan_fambro <startek591@gmail.com>
2025-12-14 16:45:08 -0500

    Separating HTML and CSS into separate files

--- angular/first-component/src/app/user-profile/user-profile.component.ts
+++ angular/first-component/src/app/user-profile/user-profile.component.ts
@@ -2,11 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'user-profile',
+  templateUrl: 'user-profile.component.html',
   standalone: true,
-  template: `
-    <h1>User profile</h1>
-    <p>This is the user profile page</p>
-  `,
-  styles: `h1 { font-size: 3em; }`,
+  styleUrl: 'user-profile.component.css',
 })
 export class UserProfile {}
