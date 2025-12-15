cd64f4d127eed7bafe6bf58afcd4cc3cd8ea2c69
jonathan_fambro <startek591@gmail.com>
2025-12-15 11:08:57 -0500

    Using Components

--- angular/first-component/src/app/user-profile/user-profile.component.ts
+++ angular/first-component/src/app/user-profile/user-profile.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-
+import { ProfilePhoto } from '../profile-photo/profile-photo.component';
 @Component({
   selector: 'user-profile',
   templateUrl: 'user-profile.component.html',
-  standalone: true,
   styleUrl: 'user-profile.component.css',
+  imports: [ProfilePhoto],
 })
 export class UserProfile {}
