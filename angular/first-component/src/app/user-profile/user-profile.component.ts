6b7802285a0eb410c2724af572b225ddfd7eac20
jonathan_fambro <startek591@gmail.com>
2025-12-15 13:30:55 -0500

    Templates and Dynamically Renders

--- angular/first-component/src/app/user-profile/user-profile.component.ts
+++ angular/first-component/src/app/user-profile/user-profile.component.ts
@@ -1,5 +1,11 @@
-import { Component, signal, computed } from '@angular/core';
+import { Component, signal, computed, OnInit } from '@angular/core';
 import { ProfilePhoto } from '../profile-photo/profile-photo.component';
+
+interface Badge {
+  id: number;
+  name: string;
+}
+
 @Component({
   selector: 'user-profile',
   templateUrl: 'user-profile.component.html',
@@ -10,8 +16,28 @@ export class UserProfile {
   isTrial = signal(false);
   isTrialExpired = signal(false);
   showTrialDuration = computed(() => this.isTrial() && !this.isTrialExpired());
+  userName = signal('pro_programmer_123');
+  userRole: 'admin' | 'user' = 'admin';
+
+  isValidUserId = signal(false);
+
+  badges = signal<Badge[]>([
+    { id: 1, name: 'Admin' },
+    { id: 2, name: 'Editor' },
+    { id: 3, name: 'Contributor' },
+  ]);
+
+  ngOnInt() {
+    this.userName.set('cool_coder_789');
+  }
+
+  isAdmin(): boolean {
+    return this.userRole === 'admin';
+  }
 
   activateTrial() {
     this.isTrial.set(true);
   }
+
+  cancelSubscription(event: Event) {}
 }
