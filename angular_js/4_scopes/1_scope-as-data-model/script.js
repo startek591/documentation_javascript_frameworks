a9b238f7cd63921a63da115d084440134decdfd9
jonathan_fambro <startek591@gmail.com>
2025-12-18 13:53:12 -0500

    Scope as Data-Model

---
+++ angular_js/4_scopes/1_scope-as-data-model/script.js
@@ -0,0 +1,12 @@
+(function () {
+  angular.module("scopeExample", []).controller("MyController", [
+    "$scope",
+    function ($scope) {
+      $scope.username = "World";
+
+      $scope.sayHello = function () {
+        $scope.greeting = "Hello " + $scope.username + "!";
+      };
+    },
+  ]);
+})();
