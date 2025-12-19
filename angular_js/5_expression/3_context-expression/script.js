0f7d103e56fdbe61704f79bf8e9101aab8745efc
jonathan_fambro <startek591@gmail.com>
2025-12-19 13:17:40 -0500

    Context Example

---
+++ angular_js/5_expression/3_context-expression/script.js
@@ -0,0 +1,13 @@
+(function () {
+  angular.module("expressionExample", []).controller("ExampleController", [
+    "$window",
+    "$scope",
+    function ($window, $scope) {
+      $scope.name = "World";
+
+      $scope.greet = function () {
+        $window.alert("Hello " + $scope.name);
+      };
+    },
+  ]);
+})();
