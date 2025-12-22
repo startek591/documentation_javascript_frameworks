7205edb6b27d4e758fc3c73dae59922fcd8b1a00
jonathan_fambro <startek591@gmail.com>
2025-12-22 11:02:54 -0500

    Creating custom filters

---
+++ angular_js/6_filters/2_creating-custom-filters/script.js
@@ -0,0 +1,26 @@
+(function () {
+  angular
+    .module("myReverseFilterApp", [])
+    .controller("MyController", [
+      "$scope",
+      "reverseFilter",
+      function ($scope, reverseFilter) {
+        $scope.greeting = "hello";
+        $scope.filterGreeting = reverseFilter($scope.greeting);
+      },
+    ])
+    .filter("reverse", function () {
+      return function (input, uppercase) {
+        input = input || "";
+        var out = "";
+        for (var i = 0; i < input.length; i++) {
+          out = input.charAt(i) + out;
+        }
+
+        if (uppercase) {
+          out = out.toUpperCase();
+        }
+        return out;
+      };
+    });
+})();
