1bf4f12e0ecb1ca519f280111145ba7742ad4dd1
jonathan_fambro <startek591@gmail.com>
2025-12-24 00:21:03 -0500

    Creating a Directive that Wraps Other Elements

---
+++ angular_js/8_directives/5_directive-wrap-other-elements/script.js
@@ -0,0 +1,18 @@
+(function () {
+  angular
+    .module("docsTransclusionDirective", [])
+    .controller("Controller", [
+      "$scope",
+      function ($scope) {
+        $scope.name = "Tobias";
+      },
+    ])
+    .directive("myDialog", function () {
+      return {
+        restrict: "E",
+        transclude: true,
+        scope: {},
+        templateUrl: "my-dialog.html",
+      };
+    });
+})();
