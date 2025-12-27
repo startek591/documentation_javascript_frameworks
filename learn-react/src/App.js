e8389b4700381e277934cd058b654e5c009ee0c3
jonathan_fambro <startek591@gmail.com>
2025-12-27 18:41:02 -0500

    Exporting and importing a component

--- learn-react/src/App.js
+++ learn-react/src/App.js
@@ -1,6 +1,12 @@
-import Gallery from "./1_component/Gallery";
+// import Gallery from "./1_component/Gallery";
+import Gallery from "./2_import_and_export/Gallery";
 function App() {
-  return <Gallery />;
+  return (
+    <>
+      {/* <Gallery />; */}
+      <Gallery />
+    </>
+  );
 }
 
 export default App;
