c6dcde4e7b82ca714fe6a5588a75a1db21a99298
jonathan_fambro <startek591@gmail.com>
2025-12-27 18:52:20 -0500

    Rules of JSX

---
+++ learn-react/src/3_writing_markups/Todo.js
@@ -0,0 +1,21 @@
+export default function TodoList() {
+  return (
+    <>
+      <h1>Hedy Lamarr's Todos</h1>
+      <img
+        src="https://i.imgur.com/yXOvdOSs.jpg"
+        alt="Hedy Lamarr"
+        className="photo"
+      />
+      <ul>
+        <li>Invent new traffic lights</li>
+        <li>Rehearse a movie scene</li>
+        <li>Improve the spectrum technology</li>
+      </ul>
+    </>
+  );
+}
+
+// Return a single root element
+// Close all the tags
+// camelCase most of the things!
--- learn-react/src/App.js
+++ learn-react/src/App.js
@@ -1,10 +1,13 @@
 // import Gallery from "./1_component/Gallery";
 import { Profile } from "./2_import_and_export/Gallery";
+import TodoList from "./3_writing_markups/Todo";
 function App() {
   return (
     <>
       {/* <Gallery />; */}
       <Profile />
+      <hr />
+      <TodoList />
     </>
   );
 }
