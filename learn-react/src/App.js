a21e06834988c368d6788b6b6925685a83eda621
jonathan_fambro <startek591@gmail.com>
2025-12-27 18:44:06 -0500

    Exporting and importing multiple components form the same file

--- learn-react/src/App.js
+++ learn-react/src/App.js
@@ -1,10 +1,10 @@
 // import Gallery from "./1_component/Gallery";
-import Gallery from "./2_import_and_export/Gallery";
+import { Profile } from "./2_import_and_export/Gallery";
 function App() {
   return (
     <>
       {/* <Gallery />; */}
-      <Gallery />
+      <Profile />
     </>
   );
 }
