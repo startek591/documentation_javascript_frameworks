e83c7a42020df56e98c8522b4666e720a720edb4
jonathan_fambro <startek591@gmail.com>
2025-12-27 19:02:28 -0500

    JavaScript expression including function calls

--- learn-react/src/App.js
+++ learn-react/src/App.js
@@ -1,16 +1,19 @@
 // import Gallery from "./1_component/Gallery";
 import { Profile } from "./2_import_and_export/Gallery";
-import TodoList from "./3_writing_markups/Todo";
+// import TodoList from "./3_writing_markups/Todo";
 import Avatar from "./4_javascript_with_curly_braces/Avatar";
+import TodoList from "./4_javascript_with_curly_braces/TodoList";
 function App() {
   return (
     <>
       {/* <Gallery />; */}
       <Profile />
       <hr />
-      <TodoList />
+      {/* <TodoList /> */}
       <hr />
       <Avatar />
+      <hr />
+      <TodoList />
     </>
   );
 }
