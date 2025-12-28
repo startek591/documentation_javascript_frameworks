ef07eaec8bfbee02a36e43b082d4cfb0d9b57cab
jonathan_fambro <startek591@gmail.com>
2025-12-28 17:33:29 -0500

    Passing JSX as children

--- learn-react/src/App.js
+++ learn-react/src/App.js
@@ -3,7 +3,25 @@
 // import TodoList from "./3_writing_markups/Todo";
 // import Avatar from "./4_javascript_with_curly_braces/Avatar";
 // import TodoList from "./4_javascript_with_curly_braces/TodoList";
-import Profile from "./5_passing_props_to_components/Profile";
+import Avatar from "./5_passing_props_to_components/Avatar";
+
+function Card({ children }) {
+  return <div className="card">{children}</div>;
+}
+
+function Profile() {
+  return (
+    <Card>
+      <Avatar
+        size={100}
+        person={{
+          name: "Katsuko Saruhashi",
+          imageId: "YfeOqp2",
+        }}
+      />
+    </Card>
+  );
+}
 function App() {
   return (
     <>
