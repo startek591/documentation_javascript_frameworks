7e89df3347de35ea5f0a1643876cd811b1df52f0
jonathan_fambro <startek591@gmail.com>
2025-12-28 17:36:43 -0500

    How props change over time

--- learn-react/src/App.js
+++ learn-react/src/App.js
@@ -3,25 +3,26 @@
 // import TodoList from "./3_writing_markups/Todo";
 // import Avatar from "./4_javascript_with_curly_braces/Avatar";
 // import TodoList from "./4_javascript_with_curly_braces/TodoList";
-import Avatar from "./5_passing_props_to_components/Avatar";
+// import Avatar from "./5_passing_props_to_components/Avatar";
+import Clock from "./5_passing_props_to_components/Clock";
 
-function Card({ children }) {
-  return <div className="card">{children}</div>;
-}
+// function Card({ children }) {
+//   return <div className="card">{children}</div>;
+// }
 
-function Profile() {
-  return (
-    <Card>
-      <Avatar
-        size={100}
-        person={{
-          name: "Katsuko Saruhashi",
-          imageId: "YfeOqp2",
-        }}
-      />
-    </Card>
-  );
-}
+// function Profile() {
+//   return (
+//     <Card>
+//       <Avatar
+//         size={100}
+//         person={{
+//           name: "Katsuko Saruhashi",
+//           imageId: "YfeOqp2",
+//         }}
+//       />
+//     </Card>
+//   );
+// }
 function App() {
   return (
     <>
@@ -33,7 +34,8 @@ function App() {
       {/* <Avatar /> */}
       {/* <hr /> */}
       {/* <TodoList /> */}
-      <Profile />
+      {/* <Profile /> */}
+      <Clock />
     </>
   );
 }
