5304d3489e1d1d3d576574518164c785f371e08f
jonathan_fambro <startek591@gmail.com>
2025-12-28 16:35:58 -0500

    Pass props to the child component

--- learn-react/src/App.js
+++ learn-react/src/App.js
@@ -1,19 +1,21 @@
 // import Gallery from "./1_component/Gallery";
-import { Profile } from "./2_import_and_export/Gallery";
+// import { Profile } from "./2_import_and_export/Gallery";
 // import TodoList from "./3_writing_markups/Todo";
-import Avatar from "./4_javascript_with_curly_braces/Avatar";
-import TodoList from "./4_javascript_with_curly_braces/TodoList";
+// import Avatar from "./4_javascript_with_curly_braces/Avatar";
+// import TodoList from "./4_javascript_with_curly_braces/TodoList";
+import Profile from "./5_passing_props_to_components/Profile";
 function App() {
   return (
     <>
       {/* <Gallery />; */}
-      <Profile />
-      <hr />
+      {/* <Profile /> */}
+      {/* <hr /> */}
       {/* <TodoList /> */}
-      <hr />
-      <Avatar />
-      <hr />
-      <TodoList />
+      {/* <hr /> */}
+      {/* <Avatar /> */}
+      {/* <hr /> */}
+      {/* <TodoList /> */}
+      <Profile />
     </>
   );
 }
